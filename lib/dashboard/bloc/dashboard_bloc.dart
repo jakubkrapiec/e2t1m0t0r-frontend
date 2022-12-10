e354ff09b005d2d3202a5549fc887d03976fecc3
SugaR256 <kuba.krapiec@outlook.com>
2022-12-10 15:02:38 +0100

    fix a bug caused by a deprecated endpoint

--- lib/dashboard/bloc/dashboard_bloc.dart
+++ lib/dashboard/bloc/dashboard_bloc.dart
@@ -14,40 +14,21 @@ class DashboardBloc extends Bloc<DashboardEvent, DashboardState> {
     emit(DashboardState.fromValues({}, DashboardProcessingStage.loading));
     final dio = Dio();
 
-    if (event.models.length == 1) {
-      final formData = FormData.fromMap({
-        'file': MultipartFile.fromBytes(event.models.values.single, filename: event.models.keys.single),
-      });
+    final formDataMap = <String, dynamic>{'files_number': event.models.length};
+    for (var i = 0; i < event.models.length; i++) {
+      formDataMap.putIfAbsent(
+        'file_$i',
+        () => MultipartFile.fromBytes(event.models.values.toList()[i], filename: event.models.keys.toList()[i]),
+      );
+    }
 
-      final response = await dio.post<String>('$_apiUrl/estimate', data: formData);
-      if (response.statusCode == 200) {
-        final body = jsonDecode(response.data!) as Map;
-        emit(
-          DashboardState.fromValues(
-            {event.models.keys.single: body['duration'] as int},
-            DashboardProcessingStage.idle,
-          ),
-        );
-      } else {
-        emit(DashboardState.fromValues({'error': 0}, DashboardProcessingStage.error));
-      }
+    final formData = FormData.fromMap(formDataMap);
+    final response = await dio.post<String>('$_apiUrl/estimate_multiple', data: formData);
+    if (response.statusCode == 200) {
+      final body = (jsonDecode(response.data!) as Map).cast<String, int>();
+      emit(DashboardState.fromValues(body, DashboardProcessingStage.idle));
     } else {
-      final formDataMap = <String, dynamic>{'files_number': event.models.length};
-      for (var i = 0; i < event.models.length; i++) {
-        formDataMap.putIfAbsent(
-          'file_$i',
-          () => MultipartFile.fromBytes(event.models.values.toList()[i], filename: event.models.keys.toList()[i]),
-        );
-      }
-
-      final formData = FormData.fromMap(formDataMap);
-      final response = await dio.post<String>('$_apiUrl/estimate_multiple', data: formData);
-      if (response.statusCode == 200) {
-        final body = (jsonDecode(response.data!) as Map).cast<String, int>();
-        emit(DashboardState.fromValues(body, DashboardProcessingStage.idle));
-      } else {
-        emit(DashboardState.fromValues({'error': 0}, DashboardProcessingStage.error));
-      }
+      emit(DashboardState.fromValues({'error': 0}, DashboardProcessingStage.error));
     }
   }
 
